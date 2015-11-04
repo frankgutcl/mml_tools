789abba73a6f7b79315da0bcb70fc6ce945a504e
zhiming.tang <me@tangzm.com>
2015-11-04 11:57:43 +0800

    Re-organize the report generation

--- toolbox/gen_oecf_report.m
+++ toolbox/gen_oecf_report.m
@@ -5,8 +5,10 @@
 % * rgbw_array: The rgbw of patches(captured) in captured photos
 % * label_array: The labels of files
 % * lumi_table: The luminance table (measured)
+% * pic_files: The pictures (with cropped patches)
+% * pic_ratios: The ratio of the pictures
 
-function gen_oecf_report(file, rgbw_array, label_array, lumi_table)
+function gen_oecf_report(file, rgbw_array, label_array, lumi_table, pic_files, pic_ratios)
     T1 = [3 2 6 22];
     T2 = [2 2 2 22];
     
@@ -44,13 +46,22 @@ function gen_oecf_report(file, rgbw_array, label_array, lumi_table)
         for i=1:20
             sheet.Range(getRange(1,T1(2)+i,1,T1(2)+i)).Interior.Color = RGB(rgbw_array(index,i,1), rgbw_array(index,i,2), rgbw_array(index,i,3));
         end
+        
+        left = excel.ActiveSheet.Range('G1').Left;
+        top = excel.ActiveSheet.Range('G1').Top;
+        sheet.Shapes.AddPicture(cell2mat(pic_files(index)),0,1,floor(left),floor(top), floor(350/pic_ratios(index)), 350);
         sheet.Name = cell2mat(label_array(index));
     end
     
-    %Adding chart
-    sheet = sheets.Item(1); 
+    %Adding sheet for summary
+    sheet = sheets.Item(1);
+    sheet.Activate;
+    sheet = sheets.Add([]);    
     sheet.Activate; 
-    chart = excel.ActiveSheet.Shapes.AddChart(); 
+    sheet.Name = 'summary';
+    
+    %Adding chart
+    chart = excel.ActiveSheet.Shapes.AddChart();
     chart.Name = 'OECF';
 
     ExpChart = excel.ActiveSheet.ChartObjects('OECF');
@@ -73,16 +84,40 @@ function gen_oecf_report(file, rgbw_array, label_array, lumi_table)
         NewSeries.Values  = [cell2mat(label_array(index)) '!F' int2str(3) ':F' int2str(22)];
         NewSeries.Name    = cell2mat(label_array(index));
     end
-    excel.ActiveChart.ChartType = 'xlXYScatterLinesNoMarkers'; 
+    excel.ActiveChart.ChartType = 'xlXYScatterSmoothNoMarkers'; 
+    excel.ActiveChart.HasTitle = 1;
+    excel.ActiveChart.ChartTitle.Characters.Text = 'OECF';
+    
+    % Setting the (X-Axis) and (Y-Axis) titles. 
+    ChartAxes = chart.Chart.Axes(1); 
+    set(ChartAxes,'HasTitle',1); 
+    set(ChartAxes.AxisTitle,'Caption','Luminance - cd/m2'); 
+    ChartAxes = chart.Chart.Axes(2);  
+    set(ChartAxes,'HasTitle',1); 
+    set(ChartAxes.AxisTitle,'Caption','Response Level'); 
 
+    % Setting the (Axis) Scale 
+    excel.ActiveChart.Axes(1).Select; 
+    excel.ActiveChart.Axes(1).MinimumScale = 0;             
+    excel.ActiveChart.Axes(1).MaximumScale = 600; 
+    
+    excel.ActiveChart.Axes(2).Select; 
+    excel.ActiveChart.Axes(2).MinimumScale = 0;             
+    excel.ActiveChart.Axes(2).MaximumScale = 256; 
+    
+    %Placement
+    GetPlacement = get(excel.ActiveSheet,'Range', 'B17');
+    ExpChart.Left = GetPlacement.Left;
+    ExpChart.Top = GetPlacement.Top;
     
     sheet = sheets.Item(1); 
     sheet.Activate; 
     chart = excel.ActiveSheet.Shapes.AddChart(); 
-    chart.Name = 'Tone Curve';
+    chart.Name = 'Tone';
 
-    ExpChart = excel.ActiveSheet.ChartObjects('Tone Curve');
+    ExpChart = excel.ActiveSheet.ChartObjects('Tone');
     ExpChart.Activate;
+
 
     %Clear the original data
     try
@@ -120,8 +155,31 @@ function gen_oecf_report(file, rgbw_array, label_array, lumi_table)
         NewSeries.Values  = gray(1:max);
         NewSeries.Name    = cell2mat(label_array(index));
     end
-    excel.ActiveChart.ChartType = 'xlXYScatterLinesNoMarkers'; 
+    excel.ActiveChart.ChartType = 'xlXYScatterSmoothNoMarkers';
+    excel.ActiveChart.HasTitle = 1;
+    excel.ActiveChart.ChartTitle.Characters.Text = 'Tone Curve';
 
+    % Setting the (X-Axis) and (Y-Axis) titles. 
+    ChartAxes = chart.Chart.Axes(1); 
+    set(ChartAxes,'HasTitle',1); 
+    set(ChartAxes.AxisTitle,'Caption','Stimulus'); 
+    ChartAxes = chart.Chart.Axes(2);  
+    set(ChartAxes,'HasTitle',1); 
+    set(ChartAxes.AxisTitle,'Caption','Response'); 
+
+    % Setting the (Axis) Scale 
+    excel.ActiveChart.Axes(1).Select; 
+    excel.ActiveChart.Axes(1).MinimumScale = 0;             
+    excel.ActiveChart.Axes(1).MaximumScale = 1025; 
+    
+    excel.ActiveChart.Axes(2).Select; 
+    excel.ActiveChart.Axes(2).MinimumScale = 0;             
+    excel.ActiveChart.Axes(2).MaximumScale = 256; 
+    %Placement
+    GetPlacement = get(excel.ActiveSheet,'Range', 'B2');
+    ExpChart.Left = GetPlacement.Left;
+    ExpChart.Top = GetPlacement.Top;
+    
     
     workbook.Save();
     workbook.Close();
--- toolbox/test_oecf.m
+++ toolbox/test_oecf.m
@@ -1,7 +1,7 @@
 %To exam the oecf(Optical-Electronic-Conversion-Function) of the camaera
 %
 % Input 
-% *in_file: A string of file name(path) or an array of files
+% *in_file: A string of file name(path) or an array of files (embrace with {} and seperate with ;)
 %
 % Output
 % *out_file: The report to be generated
@@ -15,6 +15,8 @@ function test_oecf(in_file, out_file)
     
     rgbw_array = ones(size(in_file,1), 20, 4);
     label_array = {};
+    picture_files = {};
+    picture_ratios = [];
     
     for i=1:size(in_file,1)
         fn = cell2mat(in_file(i));
@@ -27,6 +29,12 @@ function test_oecf(in_file, out_file)
         for j=1:20
             draw_rect(grid(j,1), grid(j,2), grid(j,3), grid(j,4));
         end
+        temp_pic_file = ['D:\tmp\temp' int2str(i) '.jpg'];
+        saveas(gcf, temp_pic_file);
+        picture_files(i) = mat2cell(temp_pic_file);
+        picture_ratios(i) = size(oecf,1)/size(oecf,2);
+        
+        close;
 
         rgbw = ones(20,4);
 
@@ -37,14 +45,14 @@ function test_oecf(in_file, out_file)
         
         rgbw_array(i,:,:) = rgbw(:,:);
         
-        
-        fn = cell2mat(regexp(fn, '\w*.jpg', 'match', 'ignorecase'));
-        label_array(i) = regexp(fn, '\w*\.', 'match');        
+        splitted_path = regexp(fn, '\', 'split');
+        splitted_fn = regexp(cell2mat(splitted_path(end)), '\.', 'split');
+        label_array(i) = splitted_fn(1);        
     end
         
     ltable = load('oecf_lum.dat');
     
-    gen_oecf_report(out_file, rgbw_array, label_array, ltable);
+    gen_oecf_report(out_file, rgbw_array, label_array, ltable, picture_files, picture_ratios);
     %plot_oecf_curv(rgbw, ltable);
     
     disp('Done!');
