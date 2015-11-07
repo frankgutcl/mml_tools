39827e41012761cfa67272b03f7c9bf0f082eb77
zhiming.tang <me@tangzm.com>
2015-11-07 09:32:40 +0800

    Be able to decode the m10 and q10
    Export the 10bit data (with uint16)

--- toolbox/read_qualcomm_raw.m
+++ toolbox/read_qualcomm_raw.m
@@ -13,28 +13,41 @@ function ret=read_qualcomm_raw(file, w, h, format)
 
     %w = 4212;
     %h = 3120;
-
+    
+    
     if strcmpi(format, 'm10')
         %Not applicable yet
-        size = (w*h*5)/4;
-        Data = fread(fp,size,'*uint8', 0,'l' );
-
-        ret = reshape(Data, [5 size/5]);
-        ret = reshape(ret(2:5, :), [w h]);
+        sizet = w*h*5/4 + h*4;
+        Data = fread(fp,sizet,'*uint8', 0,'l' );
+        Data = uint16(Data);
+        
+        for row=1:h
+            row_offset = (row-1)*(w*5/4+4);
+            
+            for col=1:4:w
+                col_offset = floor(col/4) * 5;
+                total_offset = row_offset + col_offset + 1;
+                ret(row, col  ) = bitor(bitshift(Data(total_offset  ),2), bitand(bitshift(Data(total_offset+4),0),3));
+                ret(row, col+1) = bitor(bitshift(Data(total_offset+1),2), bitand(bitshift(Data(total_offset+4),-2),3));
+                ret(row, col+2) = bitor(bitshift(Data(total_offset+2),2), bitand(bitshift(Data(total_offset+4),-4),3));
+                ret(row, col+3) = bitor(bitshift(Data(total_offset+3),2), bitand(bitshift(Data(total_offset+4),-6),3));
+            end
+        end
+  
         fclose(fp);
     elseif strcmpi(format,'q10')
-        size = w*h/6;
-        Data = fread(fp,size,'*uint64', 0,'l');
+        sizet = w*h/6;
+        Data = fread(fp,sizet,'*uint64', 0,'l');
 
-        image(1,1:size) = uint16(bitand(Data(1:size),1023));
-        image(2,1:size) = uint16(bitshift(bitand(Data(1:size),2^20 - 2^10),-10));
-        image(3,1:size) = uint16(bitshift(bitand(Data(1:size),2^30 - 2^20),-20));
-        image(4,1:size) = uint16(bitshift(bitand(Data(1:size),2^40 - 2^30),-30));
-        image(5,1:size) = uint16(bitshift(bitand(Data(1:size),2^50 - 2^40),-40));
-        image(6,1:size) = uint16(bitshift(bitand(Data(1:size),2^60 - 2^50),-50));
+        image(1,1:sizet) = uint16(bitand(Data(1:sizet),1023));
+        image(2,1:sizet) = uint16(bitshift(bitand(Data(1:sizet),2^20 - 2^10),-10));
+        image(3,1:sizet) = uint16(bitshift(bitand(Data(1:sizet),2^30 - 2^20),-20));
+        image(4,1:sizet) = uint16(bitshift(bitand(Data(1:sizet),2^40 - 2^30),-30));
+        image(5,1:sizet) = uint16(bitshift(bitand(Data(1:sizet),2^50 - 2^40),-40));
+        image(6,1:sizet) = uint16(bitshift(bitand(Data(1:sizet),2^60 - 2^50),-50));
         img1 = reshape(image,[w h]);
-        img = img1';
-        ret = uint8(bitshift(img,-(10-8)));
+        ret = img1';
+        %ret = uint8(bitshift(img,-(10-8)));
         fclose(fp);
     end
 end
