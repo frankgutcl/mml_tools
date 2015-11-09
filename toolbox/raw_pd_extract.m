b89ec56bc8743010f7215c929e8afa55e2aab33a
zhiming.tang <me@tangzm.com>
2015-11-09 21:57:56 +0800

    Fix an error of pd map generate

--- toolbox/raw_pd_extract.m
+++ toolbox/raw_pd_extract.m
@@ -2,7 +2,7 @@
 %
 % Input
 % * raw_image: The raw image 
-% * pd_pattern: The PD pattern, currently only support "3m2"
+% * pd_pattern: The PD pattern, currently only support '3m2'
 % 
 % Output
 % * left: The image for left-covered
@@ -18,8 +18,8 @@ function [left, right, comp_raw] = raw_pd_extract(raw_image, pd_pattern)
         return;
     end
     
-    left = zeros(pd.repeat.y, pd.repeat.x);
-    right = zeros(pd.repeat.y, pd.repeat.x);
+    left = zeros(pd.repeat.y*size(pd.pattern,1), pd.repeat.x*size(pd.pattern,2));
+    right = zeros(pd.repeat.y*size(pd.pattern,1), pd.repeat.x*size(pd.pattern,2));
     
     comp_raw = raw_image;
     
@@ -30,14 +30,14 @@ function [left, right, comp_raw] = raw_pd_extract(raw_image, pd_pattern)
                     x = pd.margin.x + ((col-1)*size(pd.pattern, 2) + j-1)*pd.block.x + pd.left_pat(pd.pattern(i,j), 1);
                     y = pd.margin.y + ((row-1)*size(pd.pattern, 1) + i-1)*pd.block.y + pd.left_pat(pd.pattern(i,j), 2);               
 
-                    left(row,col) = raw_image(y,x);
+                    left((row-1)*size(pd.pattern,1)+i,(col-1)*size(pd.pattern,2)+j) = raw_image(y,x);
                     
                     comp_raw(y,x) = mean([raw_image(y, x-2), raw_image(y, x+2), raw_image(y-2, x), raw_image(y+2, x)]);
 
                     x = pd.margin.x + ((col-1)*size(pd.pattern, 2) + j-1)*pd.block.x + pd.right_pat(pd.pattern(i,j), 1);
                     y = pd.margin.y + ((row-1)*size(pd.pattern, 1) + i-1)*pd.block.y + pd.right_pat(pd.pattern(i,j), 2);
                     
-                    right(row,col) = raw_image(y, x);
+                    right((row-1)*size(pd.pattern,1)+i, (col-1)*size(pd.pattern,2)+j) = raw_image(y, x);
 
                     comp_raw(y, x) = mean([raw_image(y, x-2), raw_image(y, x+2), raw_image(y-2, x), raw_image(y+2, x)]);
                 end
